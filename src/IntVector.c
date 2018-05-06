7ef783cc16543a99fd4d54ebac053c4f4393b598
Nnobius <jackoskelle@gmail.com>
2018-05-06 23:44:26 +0700

    codestile

--- src/IntVector.c
+++ src/IntVector.c
@@ -4,12 +4,11 @@
 
 IntVector *int_vector_new(size_t initial_capacity)
 {
-    IntVector *mass = malloc ( sizeof ( IntVector ) );
+	IntVector *mass = malloc ( sizeof ( IntVector ) );
 	if (mass == NULL) {
-	    printf ("\nInt_vector_new failed");
+		printf ("\nInt_vector_new failed");
 		return NULL;
 	}
-
 	mass -> capacity = initial_capacity;
 	mass -> begin = malloc ( mass -> capacity * sizeof ( int ) );	
 	if (mass -> begin == NULL) {
@@ -17,7 +16,7 @@ IntVector *int_vector_new(size_t initial_capacity)
 		return NULL;
 	}
 	mass -> size = 0;
-    printf ("\nReady\n");
+	printf ("\nReady\n");
 	return mass;
 }
 
@@ -29,17 +28,14 @@ IntVector *int_vector_copy(const IntVector *v)
 		return NULL;
 	}
 	mass -> capacity = v -> capacity;	
-
 	mass -> begin = malloc ( v -> capacity * sizeof ( int ) );
 	if ( mass -> begin == NULL) {
 		printf ("\nint_vector_copy failed");
 		return NULL;
 	}
-
 	for ( size_t i = 0; i < v -> capacity; i++) {
 		mass -> begin [i] = v -> begin [i];
 	}
-
 	mass -> size = v -> size; 	
 	return mass;
 }
@@ -67,9 +63,7 @@ void int_vector_set_item (IntVector *v, size_t index, int item)
 		printf  ("\nint_vector_set_item failed");
 		return;
 	}
-	
 	v -> size ++;
-
 	v -> begin [index] = item;
 }
 size_t int_vector_get_size(const IntVector *v) 
@@ -90,18 +84,16 @@ int int_vector_push_back(IntVector *v, int item)
 		return 0;
 	} else {
 		printf ("\nExpanding vector with x2 capacity\n");
-
 		v -> begin = realloc ( v -> begin, v -> capacity * 2 * sizeof ( int ) );
 		if ( v == NULL ) {
 			printf ("Error of reallocation\n");
 			return -1;
 		}
-
 		v -> capacity = v -> capacity * 2;
 		v -> begin [ v -> size ] = item;
 		v -> size ++;
 		return 0;
-		}
+	}
 	return -1;
 }
 void int_vector_pop_back ( IntVector *v )
@@ -110,7 +102,6 @@ void int_vector_pop_back ( IntVector *v )
 		printf ("Vector size = 0");
 		return;
 	}
-
 	v -> begin [ v -> size - 1] = 0;
 	v -> size --;
 }
@@ -122,45 +113,34 @@ int int_vector_shrink_to_fit ( IntVector *v )
 		printf ("Reallocation error");
 		return -1;
 	}
-
 	v -> capacity = v -> size;
-
 	return 0;
 }
 
 int int_vector_resize ( IntVector *v, size_t new_size )
 {
 	if ( new_size > v -> capacity ) {
-
 		v -> begin = realloc ( v -> begin, new_size * sizeof ( int ) );
 		if ( v == NULL) {
 			printf ("Reallocation error");
 			return -1;
 		}
-
 		v -> capacity = new_size;
-
 		for ( size_t i = v -> size; i < v -> capacity; i++) {
 			v -> begin [ i ] = 0;
 		}
-
 		v -> size = new_size;
-
 		return 0;
 	}
-
 	if ( new_size > v -> size && new_size <= v -> capacity ) {
 		for ( size_t i = v -> size; i < new_size; i++) {
 			v -> begin [i] = 0;
 		}
-
 		v -> size = new_size;
 	}
-
 	if ( new_size < v -> size ) {
 		return int_vector_shrink_to_fit ( v );
 	}
-
 	return -1;
 }
 
@@ -172,15 +152,12 @@ int int_vector_reserve ( IntVector *v, size_t new_capacity )
 			printf ( "Reallocation error");
 			return -1;
 		}
-
 		v -> capacity = new_capacity;
-
 		return 0;
 	}else{
 		printf ("New size smaller or equals then previous");
 		return 0;
 	}
-	
 	return -1;
 }
 
@@ -190,7 +167,6 @@ void vector_print ( IntVector *v)
 		printf ("\nNULL\n");
 		return;
 	}
-	
 	for ( size_t i = 0; i < v -> size; i++) {
 		printf ("%d ", v -> begin [i]);
 	}
